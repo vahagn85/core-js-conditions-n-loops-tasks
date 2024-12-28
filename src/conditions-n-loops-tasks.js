1e3e2346cd5534d2bf82a0e811f32f41e3396aaf
vahagn85 <v.ulikhanyan@yandex.ru>
2024-12-28 21:05:00 +0400

    feat: implement solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -303,8 +303,44 @@ function getBalanceIndex(arr) {
  *          [10, 9,  8,  7]
  *        ]
  */
-function getSpiralMatrix(/* size */) {
-  throw new Error('Not implemented');
+function getSpiralMatrix(size) {
+  const matrix = [];
+
+  for (let i = 0; i < size; i += 1) {
+    matrix[i] = [];
+    for (let j = 0; j < size; j += 1) {
+      matrix[i][j] = 0;
+    }
+  }
+  let top = 0;
+  let bottom = size - 1;
+  let left = 0;
+  let right = size - 1;
+
+  let num = 1;
+  while (num <= size ** 2) {
+    for (let i = left; i <= right; i += 1) {
+      matrix[top][i] = num;
+      num += 1;
+    }
+    top += 1;
+    for (let i = top; i <= bottom; i += 1) {
+      matrix[i][right] = num;
+      num += 1;
+    }
+    right -= 1;
+    for (let i = right; i >= left; i -= 1) {
+      matrix[bottom][i] = num;
+      num += 1;
+    }
+    bottom -= 1;
+    for (let i = bottom; i >= top; i -= 1) {
+      matrix[i][left] = num;
+      num += 1;
+    }
+    left += 1;
+  }
+  return matrix;
 }
 
 /**
@@ -430,8 +466,37 @@ function shuffleChar(str, iterations) {
  * @param {number} number The source number
  * @returns {number} The nearest larger number, or original number if none exists.
  */
-function getNearestBigger(/* number */) {
-  throw new Error('Not implemented');
+function getNearestBigger(number) {
+  const arr = [];
+  let temp = number;
+  while (temp > 0) {
+    arr.unshift(temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+
+  let idx = arr.length - 2;
+  while (idx >= 0 && arr[idx] >= arr[idx + 1]) {
+    idx -= 1;
+  }
+  if (!idx) return number;
+
+  let j = arr.length - 1;
+  while (arr[j] <= arr[idx]) {
+    j -= 1;
+  }
+  [arr[idx], arr[j]] = [arr[j], arr[idx]];
+
+  const right = arr.filter((_, i) => i > idx);
+  right.sort((a, b) => a - b);
+  arr.length = idx + 1;
+  arr.push(...right);
+
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    result = result * 10 + arr[i];
+  }
+  return result;
 }
 
 module.exports = {
